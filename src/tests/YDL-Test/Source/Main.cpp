a467998d752adce45f147dd2eaa468226304cf9d
Sam Tsai <sam@i3d.io>
2019-03-17 08:12:41 +0800

    Test: add a new test case

--- src/tests/YDL-Test/Source/Main.cpp
+++ src/tests/YDL-Test/Source/Main.cpp
@@ -9,3 +9,15 @@
 #define CATCH_CONFIG_MAIN
 #include <catch.hpp>
 
+const char kTestVideoURL[] = "https://youtu.be/OYMUL48fHH4";
+
+TEST_CASE("Really download video", "[.]")
+{
+	const auto ydl_exe = File::getSpecialLocation(File::currentExecutableFile)
+		.getParentDirectory()
+		.getChildFile("youtube-dl.exe");
+	// youtube-dl.exe URL
+	const auto cmd = ydl_exe.getFullPathName() + " " + "--newline " + kTestVideoURL;
+	ChildProcess cp;
+	REQUIRE(cp.start(cmd));
+}
